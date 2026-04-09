baffd04eeed7fa487ddcfba23963cbe6fbbc6d42
swathi <battulaswathi15407@gmail.com>
2026-04-09 11:30:51 +0530

    terraform

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -1,10 +1,26 @@
 variable "project" {
-    default = "robooshop"
-    
+    default = "robooshop"   
+}
+variable "environment" {
+    default = "dev"   
 }
 
-variable "environment" {
-    default = "dev"
-    
+variable "sg_name" {
+    type= list
+    default = [
+       # Databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        # Backend
+        "catalogue", "user", "cart", "shipping", "payment",
+        # Backend ALB
+        "backend_alb",
+        # Frontend
+        "frontend",
+        # Frontend ALB
+        "frontend_alb",
+        # Bastion
+        "bastion",
+        
+    ]   
 }
 
