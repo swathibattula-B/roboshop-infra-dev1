18e990e9ff554357599d217f640e45dd956773a3
swathi <battulaswathi15407@gmail.com>
2026-04-09 14:38:32 +0530

    terraform

--- 30-bastion/locals.tf
+++ 30-bastion/locals.tf
@@ -1,10 +1,11 @@
 locals {
-    ami_id = data.aws_ami.joindevops.id
+    ami_id =  data.aws_ami.joindevops.id
     common_tags = {
-        project = var.project
-        environment = var.environment
-        terraform = "true"
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
     }
-    public_subnet_id = split(",",data.aws_ssm_parameter.public_subnet_ids.value)[0]
-    bastion_sg_id = "sg-09fd46ad01fdbe8a3"
+    # public subnet in 1a AZ
+    public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+    bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
 }
--- 30-bastion/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "bastion_sg_id" {
-  value = data.aws_ssm_parameter.bastion_sg_id.value
-}
