e44f105b3f6be4458151b5a40a4de187825bf29b
swathi <battulaswathi15407@gmail.com>
2026-04-09 11:21:03 +0530

    terraform

--- 30-bastion/data.tf
+++ 30-bastion/data.tf
@@ -21,7 +21,6 @@ data "aws_ami" "joindevops" {
 data "aws_ssm_parameter" "public_subnet_ids" {
     name = "/${var.project}/${var.environment}/public_subnet_ids"
 }
-
 data "aws_ssm_parameter" "bastion_sg_id" {
     name = "/${var.project}/${var.environment}/bastion_sg_id"
 }
