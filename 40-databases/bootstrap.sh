77359d230e59d56b634648debf84ce9608ee5faa
swathi <battulaswathi15407@gmail.com>
2026-04-10 02:32:01 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 component=$1
 environment=$2
 dnf install ansible -y
 
 cd /home/ec2-user
-git clone https://github.com/daws-88s/ansible-roboshop-roles-tf.git
+git clone https://github.com/swathibattula-B/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
