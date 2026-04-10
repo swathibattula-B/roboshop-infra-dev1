3c83817e1818aa6e1f464a5ee6ceafc4f22c7692
swathi <battulaswathi15407@gmail.com>
2026-04-10 21:26:58 +0530

    terraform

---
+++ 60-catalogue/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+component=$1
+environment=$2
+dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/swathibattula-B/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf
+git pull
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
