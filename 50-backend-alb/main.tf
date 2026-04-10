0170e661d5ded3675ce3132fdda585002537fd43
swathi <battulaswathi15407@gmail.com>
2026-04-10 20:39:48 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -5,6 +5,7 @@ resource "aws_lb" "backend_alb" {
   security_groups    = [local.backend_alb_sg_id]
   subnets            = local.private_subnet_ids
 
+  # keeping it as false, just to delete using terraform while practice
   enable_deletion_protection = false
 
   tags = merge(
@@ -19,27 +20,24 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.backend_alb.arn
   port              = "80"
   protocol          = "HTTP"
-  type = "fixed-response"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end.arn
-  }
+    type = "fixed-response"
 
-  fixed_response {
+    fixed_response {
       content_type = "text/html"
-      message_body = "<h1>hi, iam from HTTP backend_alb</h1>"
+      message_body = "<h1>Hi, I am from HTTP Backend ALB</h1>"
       status_code  = "200"
     }
-  
+  }
 }
 
 resource "aws_route53_record" "www" {
   zone_id = var.zone_id
   name    = "*.backend-alb-${var.environment}.${var.domain_name}"
   type    = "A"
-
-#load balancer details
+  
+  # load balancer details
   alias {
     name                   = aws_lb.backend_alb.dns_name
     zone_id                = aws_lb.backend_alb.zone_id
