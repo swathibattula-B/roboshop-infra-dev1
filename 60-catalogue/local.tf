f2c5741cf05d2aed7d47fe08622347708c958fc9
swathi <battulaswathi15407@gmail.com>
2026-04-10 21:30:23 +0530

    terraform

--- 60-catalogue/local.tf
+++ 60-catalogue/local.tf
@@ -1,5 +1,6 @@
 locals {
-    catalogue_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    ami_id = data.aws_ami.joindevops.id
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
     common_tags = {
         Project = var.project
