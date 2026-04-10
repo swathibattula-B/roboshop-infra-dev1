29b30311f3ac9c2111d904de7ee1649b3221bd28
swathi <battulaswathi15407@gmail.com>
2026-04-10 11:42:20 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -56,3 +56,12 @@ resource "aws_security_group_rule" "redis_user" {
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.redis_sg_id
 }
+resource "aws_security_group_rule" "mysql_user" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.mysql_sg_id
+}
