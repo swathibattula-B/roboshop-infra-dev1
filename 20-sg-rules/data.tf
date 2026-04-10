514372d157747fa7c8d72cfb3c846411542b3133
swathi <battulaswathi15407@gmail.com>
2026-04-10 14:08:30 +0530

    terraform

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -17,6 +17,9 @@ data "aws_ssm_parameter" "redis_sg_id" {
 data "aws_ssm_parameter" "mysql_sg_id" {
     name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+    name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
+}
 data "aws_ssm_parameter" "catalogue_sg_id" {
     name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
