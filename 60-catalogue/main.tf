49e794228db71f4709916e215bf8c3ee92f1db29
swathi <battulaswathi15407@gmail.com>
2026-04-11 21:47:02 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -44,7 +44,7 @@ resource "aws_ec2_instance_state" "catalogue" {
 }
 
 resource "aws_ami_from_instance" "catalogue" {
-  name               = "{var.project}-${var.environment}-catalogue"
+  name               = "${var.project}-${var.environment}-catalogue"
   source_instance_id = aws_instance.catalogue.id
   depends_on = [aws_ec2_instance_state.catalogue]
   tags = merge(
