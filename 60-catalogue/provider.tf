b1bcf77bf469e39ee2da13a272fa6762074b2b04
swathi <battulaswathi15407@gmail.com>
2026-04-11 20:40:47 +0530

    terraform

--- 60-catalogue/provider.tf
+++ 60-catalogue/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "mylife-myrule1-dev" 
-    key    = "roboshop-dev-database"
+    key    = "roboshop-dev-catalogue"
     region  = "us-east-1"
     encrypt = true
     use_lockfile   = true
