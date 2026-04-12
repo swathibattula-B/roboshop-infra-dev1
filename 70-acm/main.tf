3a51ac2b68cd9da6ab3f1989640d4034d919bcca
swathi <battulaswathi15407@gmail.com>
2026-04-13 01:39:32 +0530

    terraform

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -29,7 +29,7 @@ resource "aws_route53_record" "roboshop" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = aws_route53_zone.roboshop.zone_id
+  zone_id         = var.zone_id
 }
 
 resource "aws_acm_certificate_validation" "roboshop" {
