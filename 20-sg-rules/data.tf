1cafc1302f8c4085a1bb5dd8875f18a7306d3afc
swathi <battulaswathi15407@gmail.com>
2026-04-13 02:15:31 +0530

    terraform

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -29,3 +29,6 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
 data "aws_ssm_parameter" "user_sg_id" {
     name = "/${var.project}/${var.environment}/user_sg_id"
 }
+data "aws_ssm_parameter" "frontend_alb_sg_id" {
+    name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
+}
