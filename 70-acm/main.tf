cd0cc9581c3b4dab845665f0e7863c33e1c5a391
swathi <battulaswathi15407@gmail.com>
2026-04-13 21:40:25 +0530

    terraform

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "roboshop" {
-  domain_name       = "*.var.domain_name"
+  domain_name       = "*.${var.domain_name}"
   validation_method = "DNS"
 
   tags = merge(
---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+variable "zone_id" {
+    default = "Z031417015R1X0219EM62"
+}
+
+variable "domain_name" {
+    default = "daws88ss.online"
+}
+
