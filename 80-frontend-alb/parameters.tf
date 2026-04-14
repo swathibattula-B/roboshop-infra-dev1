3784966c5cea55e055982365c5d8eee86c76443c
swathi <battulaswathi15407@gmail.com>
2026-04-15 02:28:32 +0530

    terraform

--- 80-frontend-alb/parameters.tf
+++ 80-frontend-alb/parameters.tf
@@ -3,3 +3,10 @@ resource "aws_ssm_parameter" "frontend_alb_listener_arn" {
   type  = "String"
   value = aws_lb_listener.https.arn
 }
+resource "aws_ssm_parameter" "frontend_alb_certificate_arn" {
+  name      = "/${var.project}/${var.environment}/frontend_alb_certificate_arn"
+  type      = "String"
+  value     = "arn:aws:acm:us-east-1:846749788810:certificate/02720896-1360-4867-bdf7-2e808888e56f"
+  overwrite = true
+}
+
