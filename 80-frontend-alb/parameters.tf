0083280fbf09d0ae09a1fe0ba91aee113ad44700
swathi <battulaswathi15407@gmail.com>
2026-04-15 02:18:18 +0530

    terraform

--- 80-frontend-alb/parameters.tf
+++ 80-frontend-alb/parameters.tf
@@ -2,4 +2,5 @@ resource "aws_ssm_parameter" "frontend_alb_listener_arn" {
   name  = "/${var.project}/${var.environment}/frontend_alb_listener_arn"
   type  = "String"
   value = aws_lb_listener.https.arn
+  overwrite = true
 }
